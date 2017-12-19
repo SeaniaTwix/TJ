9be4dba9961380bb45bfec2860eeb1ca3268a8c2
Seania Twix <admin@ez.is>
2017-12-20 04:36:11 +0900

    에디터 업데이트

--- client/imports/components/editor/suggestion/suggestion-box.component.ts
+++ client/imports/components/editor/suggestion/suggestion-box.component.ts
@@ -1,2 +1,13 @@
+import { Component, Input } from '@angular/core';
 import './syntax-list';
 
+@Component({
+  selector: 'suggestion-box',
+  templateUrl: 'suggestion-box.html'
+})
+export class SuggestionBoxComponent {
+  @Input() lexed: Array<any>;
+  @Input() showAutocomplete: Boolean;
+
+  constructor() {}
+}
